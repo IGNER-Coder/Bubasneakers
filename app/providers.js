104e3b96d3dfa03c5981ee9fdae133409a0db867
IGNER-CoderYYy <ignatiusndungu3@gmail.com>
2025-11-29 12:18:35 +0300

    Fix: Remove duplicate Navbar component from app/providers.js

--- app/providers.js
+++ app/providers.js
@@ -2,14 +2,12 @@
 
 import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "@/context/CartContext";
-import Navbar from "@/components/Navbar";
 import CartDrawer from "@/components/CartDrawer";
 
 export default function Providers({ children }) {
   return (
     <SessionProvider>
       <CartProvider>
-        <Navbar />
         <div className="min-h-screen flex flex-col">
           {children}
         </div>
